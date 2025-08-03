6d2efc1ce83f222c8faa89312f677649d744555f
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2025-08-03 15:32:32 +0300

    Вывел пользователей

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $users = User::query()
+            ->select(['id', 'name', 'email'])
+            ->paginate(10);
+        return view('welcome', compact('users'));
+    }
+}
